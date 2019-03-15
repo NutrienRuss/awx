c39c5faec64b6fadf5e659cccccc6968a4a5cb80
Daniel Sami <dsami@redhat.com>
2019-03-15 09:04:41 -0400

    e2e adjustments for fixtures and WS

--- awx/ui/test/e2e/tests/test-websockets.js
+++ awx/ui/test/e2e/tests/test-websockets.js
@@ -4,9 +4,17 @@
 
 import {
     getInventorySource,
+    getOrganization,
     getProject,
-    getJob
+    getJob,
+    getUpdatedProject
 } from '../fixtures';
+
+import {
+    AWX_E2E_URL,
+    AWX_E2E_TIMEOUT_MEDIUM,
+    AWX_E2E_TIMEOUT_LONG
+} from '../settings';
 
 let data;
 const spinny = '//*[contains(@class, "spinny")]';
@@ -30,14 +38,15 @@ module.exports = {
         const resources = [
             getInventorySource('test-websockets'),
             getProject('test-websockets', 'https://github.com/ansible/test-playbooks'),
+            getOrganization('test-websockets'),
             // launch job templates once before running tests.
             getJob('test-websockets', 'debug.yml', 'test-websockets-successful', done),
             getJob('test-websockets', 'fail_unless.yml', 'test-websockets-failed', done)
         ];
 
         Promise.all(resources)
-            .then(([inventory, project, jt1, jt2]) => {
-                data = { inventory, project, jt1, jt2 };
+            .then(([inventory, project, org, jt1, jt2]) => {
+                data = { inventory, project, org, jt1, jt2 };
                 done();
             });
 
@@ -50,26 +59,44 @@ module.exports = {
     'Test job template status updates for a successful job on dashboard': client => {
         client.useXpath().findThenClick(dashboard);
         getJob('test-websockets', 'debug.yml', 'test-websockets-successful');
-        client.expect.element(spinny).to.not.be.visible.before(5000);
+        client.expect.element(spinny).to.not.be.visible.before(AWX_E2E_TIMEOUT_MEDIUM);
         client.expect.element(`${sparklineIcon}[1]${running}`)
-            .to.be.visible.before(5000);
+            .to.be.visible.before(AWX_E2E_TIMEOUT_MEDIUM);
 
         // Allow a maximum amount of 30 seconds for the job to complete.
         client.expect.element(`${successfulJt}${sparklineIcon}[1]${success}`)
-            .to.be.present.after(30000);
+            .to.be.present.after(AWX_E2E_TIMEOUT_LONG * 3);
     },
 
     'Test job template status updates for a failed job on dashboard': client => {
         client.useXpath().findThenClick(dashboard);
         getJob('test-websockets', 'fail_unless.yml', 'test-websockets-failed');
 
-        client.expect.element(spinny).to.not.be.visible.before(5000);
+        client.expect.element(spinny).to.not.be.visible.before(AWX_E2E_TIMEOUT_MEDIUM);
         client.expect.element(`${sparklineIcon}[1]${running}`)
-            .to.be.visible.before(5000);
+            .to.be.visible.before(AWX_E2E_TIMEOUT_MEDIUM);
 
         // Allow a maximum amount of 30 seconds for the job to complete.
         client.expect.element(`${failedJt}${sparklineIcon}[1]${fail}`)
-            .to.be.present.after(30000);
+            .to.be.present.after(AWX_E2E_TIMEOUT_LONG * 3);
+    },
+
+    'Test projects list blinking icon': client => {
+        client
+            .findThenClick('//at-side-nav-item[contains(@name, "PROJECTS")]')
+            .clearValue('//input[contains(@class, SmartSearch-input)]')
+            .setValue('//input[contains(@class, SmartSearch-input)]',
+                ['name.iexact:"test-websockets-project"', client.Keys.ENTER]);
+        getUpdatedProject('test-websockets');
+        client.expect.element('//i[contains(@class, "job-running")]')
+            .to.be.visible.after(AWX_E2E_TIMEOUT_MEDIUM);
+        client.expect.element('//i[contains(@class, "job-success")]')
+            .to.be.visible.after(AWX_E2E_TIMEOUT_LONG);
+    },
+
+    'Test successful job within an organization view': client => {
+        client.navigateTo(`${AWX_E2E_URL}/organizations/${data.org.id}/job_templates`);
+        getJob(namespace)
     },
 
     after: client => {
